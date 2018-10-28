df2b5726eb949133e208c75315d4457a0067145d
Michelle Cordeiro <mic321@gmail.com>
2018-10-28 13:55:12 -0300

    #01: Commit backend

---
+++ core/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . import models
+#from . import forms
+
+# Register your models here.
+
+admin.site.register(models.Slideshow)
+admin.site.register(models.Quem_Somos)
+
+#login: admin
+#password: admin1214
